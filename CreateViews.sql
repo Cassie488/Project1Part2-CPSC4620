4ee54977af6ea6498757a6a6e9157dcbe689221d
Mary Walker Felder <121898484+mwfelder@users.noreply.github.com>
2024-11-14 18:01:54 -0500

    Add files via upload

--- CreateViews.sql
+++ CreateViews.sql
@@ -7,7 +7,7 @@ CREATE VIEW ToppingPopularity AS
 	FROM topping JOIN pizza_topping ON topping.topping_TopID = pizza_topping.topping_TopID
 	ORDER BY ToppingCount;
         
-/*CREATE VIEW ProfitByPizza AS 
+CREATE VIEW ProfitByPizza AS 
 	SELECT 
 		pizza.pizza_Size AS Size,
 		pizza.pizza_CrustType AS Crust,
@@ -15,7 +15,7 @@ CREATE VIEW ToppingPopularity AS
 		DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y") AS OrderMonth
 	FROM 
 		pizza
-		JOIN ordertable ON pizza.ordertable_OrderID = ordertable.ordertable.OrderID
+		JOIN ordertable ON pizza.ordertable_OrderID = ordertable.ordertable_OrderID
 	GROUP BY
 		OrderMonth
 	ORDER BY
@@ -23,13 +23,13 @@ CREATE VIEW ToppingPopularity AS
 
 CREATE VIEW ProfitByOrderType AS 
 	SELECT 
-		ordertable_OrderType AS customerType,
-		DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y") AS OrderMonth,
-		ordertable_CustPrice AS TotalOrderPrice, 
-		ordertable_BusPrice AS TotalOrderCost, 
-		(ordertable_CustPrice - ordertable_BusPrice) AS Profit
+		customerType,
+		OrderMonth,
+		TotalOrderPrice, 
+		TotalOrderCost, 
+		Profit
 	FROM 
-			SELECT 
+			(SELECT 
 				ordertable_OrderType AS customerType,
 				DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y") AS OrderMonth,
 				ordertable_CustPrice AS TotalOrderPrice, 
@@ -38,27 +38,25 @@ CREATE VIEW ProfitByOrderType AS
 			FROM
 				ordertable
 			GROUP BY
-				customerType, OrderMonth
+				ordertable_OrderType, DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y")) TEMP1
 
 		UNION ALL
 
-			SELECT
+			(SELECT
 				NULL,
 				'Grand Total',
 				SUM(ordertable_CustPrice),
 				SUM(ordertable_BusPrice),
-				SUM(((ordertable_CustPrice - ordertable_BusPrice))
+				SUM(((ordertable_CustPrice - ordertable_BusPrice)))
 			FROM
 				ordertable
-	GROUP BY
-		customerType, OrderMonth
+			GROUP BY
+				ordertable_OrderType, DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y")) 
 	ORDER BY
-		CASE
+		(CASE
 			WHEN OrderMonth = 'Grand Total' THEN 1
 			ELSE 0
-		END,
+		END),
 		customerType, 
 		Profit;
 		
-	
-	;
