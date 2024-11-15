d7bc6b5f070f50f5f72eb40bab9313343276f494
Cassie488 <cmp4@clemson.edu>
2024-11-15 10:39:59 -0500

    foundations and correcting

--- CreateSPs.sql
+++ CreateSPs.sql
@@ -1,32 +1,49 @@
 CREATE PROCEDURE profitOfPizza()
 BEGIN
-    SELECT 
-		pizza.pizza_Size AS Size,
-		pizza.pizza_CrustType AS Crust,
-		(pizza.pizza_CustPrice - pizza.pizza_BusPrice) AS Profit,
-		DATE_FORMAT(ordertable.ordertable_OrderDateTime, "%m/%Y") AS OrderMonth
-	FROM 
-		pizza
-		JOIN ordertable ON pizza.ordertable_OrderID = ordertable.ordertable_OrderID
-	GROUP BY
-		OrderMonth
-	ORDER BY
-		Profit DESC; 
+    
 END;
 
 
 
 CREATE PROCEDURE toppingsForView()
 BEGIN
-    SELECT  
-        topping.topping_TopName AS Topping, 
-		COUNT(pizza_topping.topping_TopID) AS ToppingCount
-	FROM 
-        topping 
-        JOIN pizza_topping ON topping.topping_TopID = pizza_topping.topping_TopID
-	ORDER BY 
-        ToppingCount
+    
 END;
 
-CREATE FUNCTION getMonthYearFromDate(date1 DATE) RETURNS VARCHAR(20) DETERMINISTIC
-	RETURN(DATE_FORMAT(date1, "%m/%Y"));
+CREATE FUNCTION getMonthYearFromDate()
+BEGIN
+	
+END;
+
+CREATE FUNCTION getMonthYearFromDate()
+BEGIN
+	
+END;
+
+CREATE TRIGGER updatetriggerfor
+ON Employee
+FOR UPDATE
+AS
+
+GO;
+
+CREATE TRIGGER updatetriggerfor
+ON Employee
+FOR UPDATE
+AS
+
+GO;
+
+CREATE TRIGGER inserttriggerfor
+ON Employee
+FOR INSERT
+AS
+   
+GO
+
+CREATE TRIGGER inserttriggerfor
+ON Employee
+FOR INSERT
+AS
+   
+GO
